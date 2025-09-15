91b9756831229edafe37f6beb8524ee70b28d525
Jorge Andres Echavarria Pardo <jorgean0827@gmail.com>
2025-09-14 19:07:48 -0500

    Estructura Inicial del proyecto

---
+++ src/main/java/com/jorgeechavarria/stationery/management_stationery/ManagementStationeryApplication.java
@@ -0,0 +1,14 @@
+package com.jorgeechavarria.stationery.management_stationery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ManagementStationeryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ManagementStationeryApplication.class, args);
+	}
+
+}
+
---
+++ src/test/java/com/jorgeechavarria/stationery/management_stationery/ManagementStationeryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jorgeechavarria.stationery.management_stationery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ManagementStationeryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
